c44488b7d8d9f5f8e6486f0fcdd93afd6d23c9b5
Nazar <zmurnazar2013@gmail.com>
2026-02-04 22:43:35 +0200

    kill me

--- js/main.js
+++ js/main.js
@@ -453,7 +453,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const header = document.querySelector('header');
     if (header) {
         window.addEventListener('scroll', () => {
-            if(window.scrollY > 50) header.classList.add('scrolled'); else header.classList.remove('scrolled');
+            if(window.scrollY > 50) header.classList.add('scrolled'); 
+            else header.classList.remove('scrolled');
         });
     }
 });
